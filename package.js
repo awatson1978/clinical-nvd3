8c13675ffabc5233ad8394df88c06be635c7e7ca
Abigail Watson <awatson1978@gmail.com>
2014-11-28 02:24:33 -0500

    bump to 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Reusable D3 graphs.',
-  version: '0.0.2',
+  version: '0.0.3',
   name: 'clinical:nvd3',
   git: 'http://github.com/awatson1978/clinical-nvd3.git'
 });
